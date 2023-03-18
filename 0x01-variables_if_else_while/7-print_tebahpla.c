f56c95196b75770aa5330ea347b6ac329146a94f
faty <fatyboubakri@31gamil.com>
2023-03-17 19:51:53 -0700

    Add print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		putchar(c);
+		c -= 1;
+	}
+	putchar('\n');
+	return (0);
+}
