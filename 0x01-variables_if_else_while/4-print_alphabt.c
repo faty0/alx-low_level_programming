15e1db0f9e7b17ed56c11ff43907c3dfa139514b
faty <fatyboubakri@31gamil.com>
2023-03-17 19:29:14 -0700

    Add print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+		c += 1;
+	}
+	putchar('\n');
+	return (0);
+}
