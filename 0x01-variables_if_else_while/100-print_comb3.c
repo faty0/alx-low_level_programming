8b36a9e31b38d6a724a78dfbe3ab69c91b20a7e0
faty <fatyboubakri@31gamil.com>
2023-03-18 08:12:31 -0700

    Add print comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c, n, d, s, dec;
+
+	c = 48;
+	n = 49;
+	d = 44;
+	s = 32;
+	dec = 49;
+	while (c <= 56)
+	{
+		while (n <= 57)
+		{
+			putchar(c);
+			putchar(n);
+			n += 1;
+			if (c != 56)
+			{
+				putchar(s);
+				putchar(d);
+			}
+		}
+		dec += 1;
+		n = dec;
+		c += 1;
+	}
+	putchar('\n');
+	return (0);
+}
