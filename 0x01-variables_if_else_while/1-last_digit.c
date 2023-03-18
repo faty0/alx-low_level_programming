cff25f1abff2bd418a5a96148a4d148cf38762be
faty <fatyboubakri@31gamil.com>
2023-03-17 17:50:19 -0700

    Update positive or negative

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,43 +1,52 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
- * Retur: Always 0 (Succeed)
+ * Return: Always 0 (Succeed)
  */
-
 int main(void)
 {
-	int n;
-	int i;
+	int n, i, j, c, isNegative;
 	float a;
-	int j;
-	char l;
-	int convertedLastdigit;
-	char stringg[10];
+	char l, stringg[10];
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	i = 1;
 	a = n;
+	isNegative = 0;
+	if (n < 0)
+	{
+		a *= -1;
+		isNegative = 1;
+	}
 	while (a > 10)
 	{
 		a = a / 10;
 		i++;
 	}
 	sprintf(stringg, "%d\n", n);
-	j = i - 1;
-	l = stringg[j];
-	convertedLastdigit = l - '0';
-	if (convertedLastdigit > 5)
-		printf("Last digit of %d is %c and it is greater than 5\n", n, l);
-	else if (convertedLastdigit == 0)
-		printf("Last digit of %d is %c and is 0\n", n, l);
+	if (isNegative == 1)
+	{
+		j = i;
+		l = stringg[j];
+		c = l - '0';
+		c *= -1;
+	} else
+	{
+		j = i - 1;
+		l = stringg[j];
+		c = l - '0';
+	}
+	if (c > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, c);
+	else if (c == 0)
+		printf("Last digit of %d is %d and is 0\n", n, c);
 	else
 	{
-		printf("Last digit of %d is %c and is less than 6 and not 0\n", n, l);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, c);
 	}
 	return (0);
 }
