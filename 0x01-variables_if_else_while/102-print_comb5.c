9d5331c3f53b9f6f9fbad26f02c5023cf84a4c4b
faty <fatyboubakri@31gamil.com>
2023-03-20 07:24:19 -0700

    Add print comb 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c, d, sp, cl;
+
+	a = 48;
+	b = 48;
+	c = 48;
+	d = 49;
+	sp = 32;
+	cl = 44;
+	while (a <= 57)
+	{
+		while (b <= 57)
+		{
+			while (c <= 57)
+			{
+				while (d <= 57)
+				{
+					if (a <= c && b < d)
+					{
+						putchar(a);
+						putchar(b);
+						putchar(sp);
+						putchar(c);
+						putchar(d);
+						if (a != 57 || b != 56 || c != 57 || d != 57)
+						{
+							putchar(cl);
+							putchar(sp);
+						}
+					}
+					d += 1;
+				}
+				c += 1;
+				d = 48;
+			}
+			b += 1;
+			c = 48;
+		}
+		a += 1;
+		b = 48;
+	}
+	putchar('\n');
+	return (0);
+}
