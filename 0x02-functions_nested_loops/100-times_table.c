0be76d585fa41624e8fa9ff79983d613c33c2756
faty <fatyboubakri@31gamil.com>
2023-03-22 12:53:06 -0700

    Update times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,39 +1,38 @@
 #include "main.h"
 /**
  * print_times_table - print n times table
- * @n: how many times 
- *
+ * @n: how many times
  * Return: void
  */
 void print_times_table(int n)
 {
-        int i, j, m, first, second, third;
+	int i, j, m, first, second, third;
 
-        i = j = 0;
-        if (n <= 15 && n >= 0)
-        {
-                while (i <= n)
-                {
-                        while (j <= n)
-                        {
-                                m = i * j;
-                                if (m < 10)
-                                {
-                                        if (j != 0)
-                                        {
-                                                _putchar(' ');
-                                                _putchar(' ');
-                                        }
-                                        _putchar('0' + m);
-                                }
-                                else if (m < 100)
-                                {
-                                        first = m / 10;
-                                        second = m - (first * 10);
-                                        _putchar(' ');
-                                        _putchar('0' + first);
-                                        _putchar('0' + second);
-                                }
+	i = j = 0;
+	if (n <= 15 && n >= 0)
+	{
+		while (i <= n)
+		{
+			while (j <= n)
+			{
+				m = i * j;
+				if (m < 10)
+				{
+					if (j != 0)
+					{
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar('0' + m);
+				}
+				else if (m < 100)
+				{
+					first = m / 10;
+					second = m - (first * 10);
+					_putchar(' ');
+					_putchar('0' + first);
+					_putchar('0' + second);
+				}
 				else
 				{
 					first = m / 100;
@@ -49,10 +48,10 @@ void print_times_table(int n)
 					_putchar(' ');
 				}
 				j += 1;
-                        }
+			}
 			_putchar('\n');
 			i += 1;
 			j = 0;
-                }
-        }
+		}
+	}
 }
