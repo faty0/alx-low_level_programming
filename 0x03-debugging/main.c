2980d5880a343231b42f2d50f36feada7f18437d
faty <fatyboubakri@31gamil.com>
2023-03-22 15:42:53 -0700

    is it still working?

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - takes a date and prints how many days are left in the year, taking
+* leap years into account
+* Return: (void)
+{
+	return (0);
+}
+
