d78bfc146aef57c42c474abb9bda60df977011ed
faty <fatyboubakri@31gamil.com>
2023-03-23 05:47:51 -0700

    Add print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print from 0-9
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 48;
+	while (i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
