1752ce164f4647793fde46dc865aebbf4dd0d07c
faty <fatyboubakri@31gamil.com>
2023-03-23 09:09:27 -0700

    Add print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - Draw a straight line
+ * @n: Number of times to print -
+ *
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
