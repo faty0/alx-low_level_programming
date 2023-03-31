439ffff67745bb852a8c64f5a0a14605bd6f4c4d
Faty <fatyboubakri31@gmail.com>
2023-03-31 00:58:46 +0000

    Add print numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_number - Prints an integer
+ * @n: the integer to print
+*/
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
