3e42e90742dd86527c0d7305a8b39cd3044e7c44
Faty <fatyboubakri31@gmail.com>
2023-04-02 21:21:13 +0000

    Fixing betty style for print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,7 +5,7 @@
 */
 void print_number(int n)
 {
-	int l,i,k,tens;
+	int l, i, k, tens;
 
 	i = 0;
 	l = n;
@@ -15,18 +15,19 @@ void print_number(int n)
 		n = -n;
 		_putchar('-');
 	}
-   while (l / 10)
-   {
-	i++;
-	l /= 10;
-   }
-   while(i >= 0)
-   {
-		for ( k = 0; k < i; k++)
+	while (l / 10)
+	{
+		i++;
+		l /= 10;
+	}
+	while (i >= 0)
+	{
+		for (k = 0; k < i; k++)
+		{
 			tens *= 10;
+		}
 		l = (n / tens) % 10;
-		_putchar(l + '0');
-	   i--;
-	   tens = 1;
+		i--;
+		tens = 1;
 	}
 }
