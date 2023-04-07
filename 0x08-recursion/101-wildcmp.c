fd31b8f3507963dd15289c8d80c264dbfee33b54
Faty <fatyboubakri31@gmail.com>
2023-04-07 16:07:25 +0000

    Fix check 14 & check 18 hopefully wwildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -43,7 +43,7 @@ int wildcmp(char *s1, char *s2)
 			return (wildcmp(s1, s2 + 1));
 		if (*(s1 + 1) == '\0' && *(s2 + 1) != '\0')
 			return (wildcmp(s1, s2 + 1));
-		if (*(s2 + 1) == '*' && *s2 == '*')
+		if (checkstring(s1, s2) && *(s2 + 1) == '*')
 			return (wildcmp(s1, s2 + 1));
 		return (wildcmp(s1 + 1, s2));
 	}
