56ac6b921048c68e7bcd30d36709a765a34eb483
Faty <fatyboubakri31@gmail.com>
2023-04-14 01:17:10 +0000

    Add calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _memset - Fills memory with constant byte
+ * @s:memory area to fill
+ * @b:constant byte
+ * @n:first bytes to fill
+ *
+ * Return: Always a pointer to memory area
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
+/***/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+
+	if (nmemb != 0 && size != 0)
+	{
+		arr = malloc(nmemb * size);
+		if (arr != NULL)
+		{
+			_memset(arr, 0, nmemb * size);
+			return (arr);
+		}
+	}
+	return (NULL);
+}
