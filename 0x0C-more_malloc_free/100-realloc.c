8fffc8ecd4d0482019c7f5a6406f0000de9589e1
Faty <fatyboubakri31@gmail.com>
2023-04-14 02:29:13 +0000

    Add 101 mul

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -6,7 +6,7 @@
  * @ptr: pointer to the memory previously allocated
  * @old_size: the size, in bytes, of the allocated space
  * @new_size: the new size, in bytes of the new memory block
- * 
+ *
  * Return: pointer to newely allocated memory
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of pointer to the arguments
+ *
+ * Return: 0 on success
+*/
+int main(int argc, char *argv[])
+{
+	return (0);
+}
