857308417c4297b4b6d8a06f173fd4fe79d77c91
Faty <fatyboubakri31@gmail.com>
2023-04-20 02:11:31 +0000

    Fix check 7 for 1-array_iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (action != NULL && size != 0)
+	if (action != NULL && size != 0 && array != NULL)
 	{
 		unsigned long int i;
 
