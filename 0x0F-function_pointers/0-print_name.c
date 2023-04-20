7b5d20d3a8c145a5ed1eff6142b2ca3928ae02ef
Faty <fatyboubakri31@gmail.com>
2023-04-20 01:35:47 +0000

    Add o-print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * print_name - Prints a name
+ * @name: the name to print
+ * @f: the function to print with
+ *
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
