987d1d0e7cb702e6a07d8629b7e0a1ba9b6e9fee
faty0 <fatyboubakri31@gmail.com>
2023-04-29 16:14:31 +0000

    Modifi 2-add_node_end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,7 +10,7 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
-	list_t *h = *head;
+	list_t *h = malloc(sizeof(list_t));
 	int l;
 
 	l = 0;
@@ -25,6 +25,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		free(h);
 		return (*head);
 	}
+	h = *head;
 	while (h->next)
 		h = h->next;
 	h->next = new;
