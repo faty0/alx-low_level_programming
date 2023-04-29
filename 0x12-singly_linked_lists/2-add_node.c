88ea91d62afa05147b41a9f3e709ade13a077398
faty0 <fatyboubakri31@gmail.com>
2023-04-29 14:36:34 +0000

    attempt 3 previous commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -28,7 +28,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
 
-	if (head == NULL)
+	if (!new)
 		return (NULL);
 	new->str = strdup(str);
 	new->len = _strlen(new->str);
