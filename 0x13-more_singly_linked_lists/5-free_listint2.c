148c153e33b90fe0948c55446cb22f817c9be4fa
faty0 <fatyboubakri31@gmail.com>
2023-05-02 18:44:02 +0000

    check 10

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,16 +5,16 @@
 */
 void free_listint2(listint_t **head)
 {
-	listint_t *next, *tofree;
+	listint_t *nex, *tofree;
 
-	if (*head == NULL || head == NULL)
+	if (!(*head) || !head)
 		return;
 	tofree = *head;
 	while (tofree)
 	{
-		next = tofree->next;
+		nex = tofree->next;
 		free(tofree);
-		tofree = next;
+		tofree = nex;
 	}
 	*head = NULL;
 }
