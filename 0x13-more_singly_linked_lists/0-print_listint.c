3188ed5c6226f8b7efa8123b2cb9af69dc745a77
faty0 <fatyboubakri31@gmail.com>
2023-05-02 13:51:28 +0000

    Initial commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * print_listint - Prints all the elements of a listint_t list
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
