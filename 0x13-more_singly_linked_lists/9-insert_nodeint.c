4aa3b35638f0003d4fd07cfab609227fd235b6f0
faty0 <fatyboubakri31@gmail.com>
2023-05-02 22:57:00 +0000

    Add null task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
-	while (nex)
+	while (nex && head && *head)
 	{
 		if (num ==idx && (nex->next))
 		{
