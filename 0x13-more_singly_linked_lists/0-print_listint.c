db49bdffcff911a7e8d47df66979ef839d32ddd6
faty0 <fatyboubakri31@gmail.com>
2023-05-02 13:58:48 +0000

    Zero task

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,14 @@
 #include "lists.h"
 /**
  * print_listint - Prints all the elements of a listint_t list
+ * @h: pointer to the list to check
+ *
+ * Return: number of elements in the list
 */
 size_t print_listint(const listint_t *h)
 {
 	size_t n = 0;
+
 	while (h)
 	{
 		printf("%d\n", h->n);
