90d19534dba300dc82af7587f29bcdccf85ad1ee
faty0 <fatyboubakri31@gmail.com>
2023-05-02 15:45:16 +0000

    Add Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - Frees a list
+ * @head: Adress of the pointer to head
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *next = *head;
+
+	while (next->next)
+	{
+		next = next->next;
+		free(*head);
+		*head = next;
+	}
+	free(*head);
+	*head = NULL;
+}
