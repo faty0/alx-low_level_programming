3ac6d8db3a8398711ceef55a4a170bbeeb24cfa5
faty0 <fatyboubakri31@gmail.com>
2023-05-03 15:26:12 +0000

    null? task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int n = 0;
 	listint_t *p, *tmp;
 
-	if (!head)
+	if (!head || !(*head))
 		return (-1);
 	p = *head;
 	if (index == 0)
