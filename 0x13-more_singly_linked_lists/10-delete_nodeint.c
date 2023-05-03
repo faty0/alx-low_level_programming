c2e6c8ebe58dd7fbfa3782943be76c82f4cc81c4
faty0 <fatyboubakri31@gmail.com>
2023-05-03 00:58:04 +0000

    task 9

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,5 +14,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (!head || !*head)
 		return (-1);
+	if (index == 0 && !(*head)->next)
+	{
+		free(*head);
+		*head = NULL;
+		return (1);
+	}
 	return (-1);
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int num = 1;
 	listint_t *new = malloc(sizeof(listint_t)), *nex = *head;
 
-	if (!head || !(*head))
+	if (!head)
 		return (NULL);
 	if (new)
 	{
