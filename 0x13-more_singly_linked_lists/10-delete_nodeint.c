ac438eebb740f5b643cae88172e3fb9305eaaec0
faty0 <fatyboubakri31@gmail.com>
2023-05-03 17:13:43 +0000

    Fix check again task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		free(*head);
-		if (!((*head)->next))
+		if (!p->next)
 			*head = NULL;
 		else
 			*head = p->next;
