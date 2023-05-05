da0f7db3498e95cc23d9debc7673f4255fe35519
faty0 <fatyboubakri31@gmail.com>
2023-05-05 21:19:09 +0000

    Add task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * set_bit - Sets the value of a bit to 1
+ * clear_bit - Sets the value of a bit to 0
  * @n: adress of number to process
  * @index:  the index of the bit to set
  *
  * Return: on success (1)
  * on faillure (-1)
 */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int bits = sizeof(*n) * 8;
 	unsigned int mask = 1;
@@ -15,7 +15,8 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index <= (bits - 1))
 	{
 		mask <<= index;
-		*n = mask | *n;
+		*n = mask ^ *n;
+		*n = *n & ~mask;
 		return (1);
 	}
 	return (-1);
