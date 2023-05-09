48c6e071fe6e0f228b4640c84c3a9bf1b27d3ff0
faty0 <fatyboubakri31@gmail.com>
2023-05-09 22:57:35 +0000

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,50 +14,56 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		printf("Usage: cp file_from file_to\n");
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd1 = open(av[1], O_RDONLY);
-	fd2 = creat(av[2], 0664);
+	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd1 < 0)
 	{
-		printf("Error: Can't read from file %s\n", av[1]);
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	if (fd2 < 0)
 	{
-		printf("Error: Can't write to %s\n", av[2]);
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 	buffer = malloc(1024);
+	if (!buffer)
+	{
+		close(fd1);
+		close(fd2);
+		dprintf(2, "Error: Failed to allocate memory\n");
+		exit(1);
+	}
 	while (r != 0)
 	{
 		r = read(fd1, buffer, 1024);
 		if (r < 0)
 		{
 			free(buffer);
-			printf("Error: Can't read from file %s\n", av[1]);
+			dprintf(2, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
-		w = write(fd2, buffer, 1024);
+		w = write(fd2, buffer, r);
 		if (w < 0)
 		{
 			free(buffer);
-			printf("Error: Can't write to %s\n", av[2]);
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
-		buffer[0] = '\0';
 	}
 	if (close(fd1) < 0)
 	{
 		free(buffer);
-		printf("Error: Can't close fd %d\n", fd1);
+		dprintf(2, "Error: Can't close fd %d\n", fd1);
 		exit(100);
 	}
 	if (close(fd2) < 0)
 	{
 		free(buffer);
-		printf("Error: Can't close fd %d\n", fd2);
+		printf(2, "Error: Can't close fd %d\n", fd2);
 		exit(100);
 	}
 	free(buffer);
