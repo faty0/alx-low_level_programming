c679c838dc4831b469f33c5d8a715f8d201279f9
faty0 <fatyboubakri31@gmail.com>
2023-05-09 18:40:16 +0000

    Fix checks task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,8 +15,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = creat(filename, S_IREAD | S_IWRITE);
 	if (fd < 0)
-		return (0);
-	w = write(fd, text_content, strlen(text_content));
+		return (-1);
+	w = write(fd, text_content, strlen(text_content) ? strlen(text_content) : 0);
 	if (w < 0)
 		return (-1);
 	close(fd);
