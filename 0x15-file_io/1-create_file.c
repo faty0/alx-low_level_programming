4cba44f1ee68d892634b4921e972b88444c3f3df
faty0 <fatyboubakri31@gmail.com>
2023-05-09 18:30:18 +0000

    Add task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,12 @@
 #include "main.h"
-/**/
+/**
+ * create_file - Creates a file
+ * @filename: the name of the file to create
+ * @text_content: the string to write to the file
+ *
+ * Return: 1 on Success
+ * -1 upon faillure
+*/
 int create_file(const char *filename, char *text_content)
 {
 	int fd, w;
