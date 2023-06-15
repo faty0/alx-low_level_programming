5e978c3ac864c5f9c09557c550130ffaca0c3365
faty <fatyboubakri@31gamil.com>
2023-06-15 12:25:31 -0700

    Fix check 11 task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,6 +16,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new = NULL;
 	dlistint_t *current;
 
+	if ((!h || !(*h)) && idx != 0)
+		return (NULL);
 	if (!h || !(*h) || idx == 0)
 	{
 		new = add_dnodeint(h, n);
