04fd1a106d67f4acdfa991644ff65961e00a2c9b
faty <fatyboubakri31@gmail.com>
2023-09-08 13:13:56 +0000

    Fix task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_t = malloc(sizeof(hash_table_t));
 	unsigned long int i;
 
-	if (new_t)
+	if (new_t && size > 0)
 	{
 		new_t->size = size;
 		new_t->array = calloc(size, sizeof(hash_node_t));
