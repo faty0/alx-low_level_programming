9a961dac2416ad98a70901b642771a554cd4cde7
faty <fatyboubakri31@gmail.com>
2023-09-15 01:01:34 +0000

    Add task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: is the hash table
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *nex;
+	int first = 1;
+
+	if (ht)
+	{
+		printf("{");
+		for (i = 0; i < ht->size; i++)
+		{
+			if (ht->array[i])
+			{
+				nex = ht->array[i];
+				while (nex)
+				{
+					if (!first)
+						printf(", ");
+					printf("'%s': '%s'", nex->key, nex->value);
+					nex = nex->next;
+					first = 0;
+				}
+			}
+		}
+		printf("}\n");
+	}
+	else
+	{
+		printf("{}");
+	}
+}
